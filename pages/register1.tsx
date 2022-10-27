bc0ec781d176e16b6368372d1455c0236944a0b4
sirapatsornt0 <sirapatsornt0@gmail.com>
2022-10-28 02:35:31 +0700

    Update register1.tsx

--- pages/register1.tsx
+++ pages/register1.tsx
@@ -3,7 +3,18 @@ import React from 'react'
 export default function register1() {
   return (
     <div>
-      <h1>Helloworld!!</h1>
+    <div>
+  <h2>HTML Forms</h2>
+  <form action="/action_page.php">
+    <label htmlFor="fname">First name:</label><br />
+    <input type="text" id="fname" name="fname" defaultValue="John" /><br />
+    <label htmlFor="lname">Last name:</label><br />
+    <input type="text" id="lname" name="lname" defaultValue="Doe" /><br /><br />
+    <input type="submit" defaultValue="Submit" />
+  </form> 
+  <p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+</div>
+
     </div>
   )
 }
