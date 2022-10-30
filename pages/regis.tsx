4f3a0853056daed07712428dbcb6a28cbe155c5d
IKNNS <kunanoon1506@gmail.com>
2022-10-30 17:18:23 +0700

    regis knn

--- pages/regis.tsx
+++ pages/regis.tsx
@@ -1,16 +1,42 @@
+import {
+    Autocomplete,
+    Box,
+    Button,
+    ImageListItem,
+    Stack,
+    TextField,
+    Typography,
+    Container
+  } from "@mui/material";
 import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Button from '@mui/material/Button';
-
-const Regis: NextPage = () => {
+import { color } from '@mui/system';
+import { colors } from '@mui/material';
+import styles2 from '../styles/asdf.module.css'
+const Home: NextPage = () => {
     return (
-        <div className={styles.container}>
-            <div>
-                Regis
+        <Container>
+        <div>
+            <Stack alignItems="center">
+            <Typography sx={{m: 4, color: "white"}} variant="h3">สมัครสมาชิค</Typography>
+            </Stack>
+            <div className="box">
+            <Stack sx={{m: 2}} spacing={3}>
+            <TextField id="outlined-basic" label="ชื่อ" variant="outlined" />
+            <TextField id="outlined-basic" label="นามสกุล" variant="outlined" />
+            <TextField id="outlined-basic" label="อีเมล์" variant="outlined" />
+            <TextField id="outlined-basic" label="รหัสผ่าน" variant="outlined" />
+            <TextField id="outlined-basic" label="ยืนยันรหัสผ่าน" variant="outlined" />
+                </Stack>
+                <Stack sx={{ width: "100%" }}  direction="row" spacing={2} >
+                <Button variant="contained" color="success">ยืนยัน</Button>
+                <Button  variant="contained" color="warning">ยกเลิก</Button>
+                </Stack>
             </div>
-            <Button variant="contained">Contained</Button>
-        </div>
+            </div>
+            </Container>
     )
 }
-
-export default Regis
+export default Home
