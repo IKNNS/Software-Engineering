6fcfebbbef615613000c45d823bd1c15a02c170e
kongsakchai <kongs.sakchai@gmail.com>
2022-11-02 00:55:59 +0700

    add goto regis

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,14 +3,16 @@ import styles from '../styles/Home.module.css'
 import Button from '@mui/material/Button';
 import { TextField } from '@mui/material';
 import { ChangeEventHandler, useState } from 'react';
+import { useRouter } from 'next/router';
 
 const Login: NextPage = () => {
+
+    const router = useRouter();
 
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
 
     const [err1, setError1] = useState<boolean>(false); // 0 : Error of username , 1 : Error of password
-
     const [err2, setError2] = useState<boolean>(false);
 
     const handleUsernameInput: ChangeEventHandler<HTMLInputElement> = (event) => {
@@ -41,16 +43,20 @@ const Login: NextPage = () => {
             console.log("OK");
     }
 
+    const goToRegis = () => {
+        router.push('/regis')
+    }
+
     return (
         <div className={` px-8 pt-10 pb-20 w-sceen h-screen flex flex-col bg-primary justify-between items-center`}>
             <div className='text-black font-bold text-3xl '>ลงชื่อเข้าใช้</div>
             <form className=' w-full flex flex-col justify-center items-center' onSubmit={(e) => { e.preventDefault(); onSubmit() }}>
                 <TextField required error={err1} value={username} onChange={handleUsernameInput} className='mb-4 bg-white' fullWidth label="Enter username" variant='outlined' />
-                <TextField required error={err2} value={password} onChange={handlePasswordInput} className='mb-2 bg-white' fullWidth label="Password" variant='outlined' type={'password'} />
-                <div className='mb-2 w-full text-right text-blue-600'>Forgot your password?</div>
+                <TextField required error={err2} value={password} onChange={handlePasswordInput} className='mb-3 bg-white' fullWidth label="Password" variant='outlined' type={'password'} />
+                <div className='mb-3 w-full text-right text-blue-600'>Forgot your password?</div>
                 <Button className='mb-4' fullWidth variant='contained' type='submit'>Sign in</Button>
                 <div className=' flex flex-row text-stone-600'>
-                    <p>Not a member?</p><p className='ml-3 text-blue-600'>Register now</p>
+                    <p>Not a member?</p><p className='ml-3 text-blue-600 hover:cursor-pointer' onClick={goToRegis}>Register now</p>
                 </div>
             </form>
         </div>
