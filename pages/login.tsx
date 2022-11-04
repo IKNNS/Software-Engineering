58d9391525b14df86f70c25f7e66f3a6d3e87c78
kongsakchai <kongs.sakchai@gmail.com>
2022-11-04 11:59:03 +0700

    Edit layout login

--- pages/login.tsx
+++ pages/login.tsx
@@ -66,39 +66,41 @@ const Login: NextPage = () => {
         router.push('/home')
         return <Loading />
     }
-    
+
     return (
 
         <div className={` w-sceen h-screen flex flex-col bg-bg justify-center items-center`}>
-            <form className=' md:w-[350px] md:h-auto h-full w-full flex flex-col justify-center items-center bg-white p-5 rounded-md shadow-md text-base' onSubmit={(e) => { e.preventDefault(); signInWithAccount() }}>
+            <form className=' md:w-[350px] md:h-auto h-full w-full flex flex-col md:justify-center justify-between items-center md:bg-white bg-transparent p-5 rounded-md shadow-md text-base' onSubmit={(e) => { e.preventDefault(); signInWithAccount() }}>
                 <div className='text-black font-medium text-2xl w-full text-center mb-10'>ลงชื่อเข้าใช้</div>
-                <TextField
-                    required error={err.email} value={emailaddress}
-                    onChange={handleEmailInput} type="email" className='mb-5 bg-white'
-                    fullWidth label="Enter username" variant='outlined'
-                />
-                <FormControl variant='outlined' required className='w-full mb-3'>
-                    <InputLabel htmlFor="password-input" error={err.password}>Password</InputLabel>
-                    <OutlinedInput
-                        id='password-input'
-                        required error={err.password} value={password}
-                        onChange={handlePasswordInput} className='bg-white'
-                        fullWidth type={viewPass ? 'text' : 'password'}
-                        label="Password"
-                        endAdornment={viewPass ?
-                            <VisibilityOffRounded className='hover:cursor-pointer' onClick={() => setViewPass(!viewPass)} /> :
-                            <VisibilityRounded className='hover:cursor-pointer' onClick={() => setViewPass(!viewPass)} />}
+                <div className='w-full'>
+                    <TextField
+                        required error={err.email} value={emailaddress}
+                        onChange={handleEmailInput} type="email" className='mb-5 bg-white'
+                        fullWidth label="Enter username" variant='outlined'
                     />
-                </FormControl>
-                <div className='mb-6 w-full text-right text-blue-600 hover:cursor-pointer' onClick={goToReset}>Forgot your password?</div>
-                <Button className='mb-1' fullWidth variant='contained' type='submit'>Sign in</Button>
-                <div className='mb-1 text-lg'>or</div>
-                <div className='flex flex-row w-full mb-6 p-2 bg-white shadow-md rounded-md border border-solid border-slate-200 items-center justify-center hover:cursor-pointer' onClick={signInWithGoogle}>
-                    <Image src={Google} width={20} height={20} alt='google-logo' />
-                    <div className='ml-3'>Sign in with Google</div>
-                </div>
-                <div className=' flex flex-row text-stone-600'>
-                    <p>Not a member?</p><p className='ml-3 text-blue-600 hover:cursor-pointer' onClick={goToRegis}>Register now</p>
+                    <FormControl variant='outlined' required className='w-full mb-3'>
+                        <InputLabel htmlFor="password-input" error={err.password}>Password</InputLabel>
+                        <OutlinedInput
+                            id='password-input'
+                            required error={err.password} value={password}
+                            onChange={handlePasswordInput} className='bg-white'
+                            fullWidth type={viewPass ? 'text' : 'password'}
+                            label="Password"
+                            endAdornment={viewPass ?
+                                <VisibilityOffRounded className='hover:cursor-pointer' onClick={() => setViewPass(!viewPass)} /> :
+                                <VisibilityRounded className='hover:cursor-pointer' onClick={() => setViewPass(!viewPass)} />}
+                        />
+                    </FormControl>
+                    <div className='mb-3 w-full text-right text-blue-600 hover:cursor-pointer' onClick={goToReset}>Forgot your password?</div>
+                    <Button className='mb-1 p-2' fullWidth variant='contained' type='submit'>Sign in</Button>
+                    <div className='mb-1 text-lg w-full text-center'>or</div>
+                    <div className='flex flex-row w-full mb-3 p-2 bg-white shadow-md rounded-md border border-solid border-slate-200 items-center justify-center hover:cursor-pointer' onClick={signInWithGoogle}>
+                        <Image src={Google} width={20} height={20} alt='google-logo' />
+                        <div className='ml-3'>Sign in with Google</div>
+                    </div>
+                    <div className=' flex flex-row text-stone-600 w-full justify-center'>
+                        <p>Not a member?</p><p className='ml-3 text-blue-600 hover:cursor-pointer' onClick={goToRegis}>Register now</p>
+                    </div>
                 </div>
             </form>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,7 @@ module.exports = {
     extend: {
       colors: {
         primary: "#ffffff",
-<<<<<<< HEAD
         bg: '#D1E8F4'
-=======
->>>>>>> d328c85fad944c5c7507085a8ca7c21c49796a52
       },
     },
   },
