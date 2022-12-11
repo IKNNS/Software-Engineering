408539ffd5a9b37501afe065054eeba13b403600
northnpk <60501616+northnpk@users.noreply.github.com>
2022-12-11 17:15:34 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ["firebasestorage.googleapis.com"]
-  }
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
